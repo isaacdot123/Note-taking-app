dad88b4d7780eb87d6685e1e18fc7c720d03cfea
Isaac Dumaplin <dumaplinisaac13@gmail.com>
2026-01-18 17:32:05 +0800

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,25 +4,35 @@ import {connectDB} from "./config/db.js";
 import rateLimiter from "../src/middleware/rateLimiter.js";
 import dotenv from "dotenv";                        //access dotenv variables
 import cors from "cors";
-
-
-
-dotenv.config();                                    //display the dotenv variables
-
-console.log(process.env.MONGO_URI);
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 
-app.use(express.json());
-app.use(cors({
+
+if(process.env.NODE_ENV !== "production"){
+    app.use(express.json());
+    app.use(cors({
     origin: "http://localhost:5173",
 }));
+}
 app.use(rateLimiter);
 
 
+
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    
+    app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+})
+
+}
+
 connectDB().then(()=> {
     app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
